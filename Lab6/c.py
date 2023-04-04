8c52064369214bed447e25781c11f3f39d8e1a6d
msobczyk-x <m.sobczyk55@wp.pl>
2023-04-04 17:37:37 +0200

    x

--- Lab6/c.py
+++ Lab6/c.py
@@ -1,5 +1,6 @@
-from threading import Semaphore
+import posix_ipc
 from multiprocessing import shared_memory
+import os
 
 
 def draw_board(arr):
@@ -24,7 +25,7 @@ def init_board_array():
 
 # initializing the board
 array = init_board_array()
-semaphore = None
+semaphore_turn = None
 shrd_mem = None
 turn = 0
 # main function
@@ -35,22 +36,19 @@ if __name__ == "__main__":
     except FileExistsError:
         shrd_mem = shared_memory.SharedMemory(name="board")
 
-    semaphore = Semaphore(1)
-
+    posix_ipc.Semaphore("./semaphore_turn", posix_ipc.O_CREAT, 0o777, 1)
     print("Welcome to Tic Tac Toe!")
 
-    draw_board(array)
     while True:
-        if semaphore.acquire():
+        
+        with semaphore_turn.acquire():
             turn = 1
             if turn:
                 print("Your turn!")
                 draw_board(array)
                 choice = input("Enter your move (X,Y): ")
-        else:
-            turn = 0
-            if not turn:
+                turn = 0
+            else:
+                turn = 0
                 print("Oponnent's turn!")
                 draw_board(array)
-                choice = input("Enter your move (X,Y): ")
-                
