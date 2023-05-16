38effe2b634f2ff396999dbdf5c83d23637a8718
msobczyk-x <m.sobczyk55@wp.pl>
2023-05-16 16:05:12 +0200

    dziala z komputerem

--- Projekt/main.py
+++ Projekt/main.py
@@ -20,6 +20,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
 
 # Draw the game board on the screen
 # Draw the game board on the screen
@@ -28,60 +29,58 @@ def draw_board():
     screen.fill(WHITE)
     for row in range(BOARD_SIZE):
         for col in range(BOARD_SIZE):
-            pygame.draw.rect(screen, BLACK, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 2)
+            # Draw the cell
+            cell_rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            
+            pygame.draw.rect(screen, BLACK, cell_rect, 1)
+            if board[row][col] == 3:
+                pygame.draw.rect(screen, GREY, cell_rect.inflate(-10, -10))
+
+            # Draw the player symbol
             if board[row][col] == 1:
-                pygame.draw.line(screen, RED, (col * CELL_SIZE + 10, row * CELL_SIZE + 10), ((col + 1) * CELL_SIZE - 10, (row + 1) * CELL_SIZE - 10), 5)
-                pygame.draw.line(screen, RED, ((col + 1) * CELL_SIZE - 10, row * CELL_SIZE + 10), (col * CELL_SIZE + 10, (row + 1) * CELL_SIZE - 10), 5)
+                pygame.draw.circle(screen, RED, cell_rect.center, CELL_SIZE // 2 - 5)
             elif board[row][col] == 2:
-                pygame.draw.circle(screen, BLUE, (col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 2 - 10, 5)
-            # Draw a grey rectangle around the 3x3 square around the player's or computer's move
-            if board[row][col] != 0:
-                pygame.draw.rect(screen, (128, 128, 128), ((col - 1) * CELL_SIZE, (row - 1) * CELL_SIZE, 3 * CELL_SIZE, 3 * CELL_SIZE))
-                if board[row][col] == 1:
-                    pygame.draw.line(screen, RED, (col * CELL_SIZE + 10, row * CELL_SIZE + 10), ((col + 1) * CELL_SIZE - 10, (row + 1) * CELL_SIZE - 10), 5)
-                    pygame.draw.line(screen, RED, ((col + 1) * CELL_SIZE - 10, row * CELL_SIZE + 10), (col * CELL_SIZE + 10, (row + 1) * CELL_SIZE - 10), 5)
-                elif board[row][col] == 2:
-                    pygame.draw.circle(screen, BLUE, (col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 2 - 10, 5)
-                    
-# Handle player moves
+                pygame.draw.rect(screen, BLUE, cell_rect.inflate(-10, -10))
+
 def player_move(row, col):
-    if board[row][col] == 0:
+    if board[row][col] == 3:
+        return False
+    elif board[row][col] == 0:
+        board[row][col] = 1
+        # Make adjacent and diagonal cells unplayable
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if 0 <= row+i < BOARD_SIZE and 0 <= col+j < BOARD_SIZE:
+                    board[row+i][col+j] = 3
         board[row][col] = 1
         return True
     else:
         return False
 
 # Check for a winner
-def check_winner():
-    # Check rows
-    for row in range(BOARD_SIZE):
-        for col in range(BOARD_SIZE - 2):
-            if board[row][col] == board[row][col+1] == board[row][col+2] != 0:
-                return board[row][col]
-    # Check columns
-    for col in range(BOARD_SIZE):
-        for row in range(BOARD_SIZE - 2):
-            if board[row][col] == board[row+1][col] == board[row+2][col] != 0:
-                return board[row][col]
-    # Check diagonals
-    for row in range(BOARD_SIZE - 2):
-        for col in range(BOARD_SIZE - 2):
-            if board[row][col] == board[row+1][col+1] == board[row+2][col+2] != 0:
-                return board[row][col]
-            if board[row][col+2] == board[row+1][col+1] == board[row+2][col] != 0:
-                return board[row][col+2]
-    # Check for tie
-    if all(cell != 0 for row in board for cell in row):
-        return 0
-    # No winner yet
-    return None
+def check_winner(player_turn):
+    if any(0 in row for row in board):
+        return None
+    else:
+        if player_turn:
+            return 1
+        else:
+            return 2
 
 # Handle computer moves
+
 def computer_move():
     empty_cells = [(row, col) for row in range(BOARD_SIZE) for col in range(BOARD_SIZE) if board[row][col] == 0]
     if empty_cells:
         row, col = random.choice(empty_cells)
+        
+        # Make adjacent cells unplayable
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if 0 <= row+i < BOARD_SIZE and 0 <= col+j < BOARD_SIZE:
+                    board[row+i][col+j] = 3
         board[row][col] = 2
+
 
 # Initialize the winner variable
 winner = None
@@ -98,14 +97,14 @@ while running:
             row = event.pos[1] // CELL_SIZE
             col = event.pos[0] // CELL_SIZE
             if player_move(row, col):
-                winner = check_winner()
+                winner = check_winner(player_turn)
                 if winner is not None:
                     running = False
                 else:
                     player_turn = False
         elif not player_turn:
             computer_move()
-            winner = check_winner()
+            winner = check_winner(player_turn)
             if winner is not None:
                 running = False
             else:
